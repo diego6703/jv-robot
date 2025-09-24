6490a99c4285015c415889eaa8b4cfa5fed9dce0
Jakub Cybulski <jakub_cybulski@o2.pl>
2025-09-24 21:39:28 +0200

    Robot app with working tests

--- src/main/java/core/basesyntax/RobotRoute.java
+++ src/main/java/core/basesyntax/RobotRoute.java
@@ -2,6 +2,47 @@ package core.basesyntax;
 
 public class RobotRoute {
     public void moveRobot(Robot robot, int toX, int toY) {
-      //write your solution here
+        int movesX = 0;
+        int movesY = 0;
+        if (toX > robot.getX()) {
+                movesX = toX - robot.getX();
+            if (toY > robot.getY()) {
+                movesY = toY - robot.getY();
+                findDirection(robot, Direction.UP);
+                goRoad(robot, movesY, movesX);
+            } else {
+                movesY = robot.getY() - toY;
+                findDirection(robot, Direction.RIGHT);
+                goRoad(robot, movesX, movesY);
+            }
+        } else {
+                movesX = robot.getX() - toX;
+            if (toY > robot.getY()) {
+                movesY = toY - robot.getY();
+                findDirection(robot, Direction.LEFT);
+                goRoad(robot, movesX, movesY);
+            } else {
+                movesY = robot.getY() - toY;
+                findDirection(robot, Direction.DOWN);
+                goRoad(robot, movesY, movesX);
+            }
+        }
+
+    }
+    public void findDirection(Robot robot, Direction direction) {
+        while (robot.getDirection() != direction) {
+            robot.turnRight();
+        }
+    }
+
+    public void goRoad(Robot robot, int goFirst, int goSecond) {
+        for (int i = 0; i < goFirst; i++) {
+            robot.stepForward();
+        }
+        robot.turnRight();
+        for (int i = 0; i < goSecond; i++) {
+            robot.stepForward();
+        }
+
     }
 }
